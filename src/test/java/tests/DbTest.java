957cb9e6ca2e76051a9833e3ec3d43a62d871649
Sergey534 <serg98_2016@mail.ru>
2023-01-25 22:57:11 +0300

    Added request 3

--- src/test/java/tests/DbTest.java
+++ src/test/java/tests/DbTest.java
@@ -12,19 +12,26 @@ public class DbTest {
 
   @Test
   public void DbTest() {
-
+    MyLogger.info("DbTest start");
     MyLogger.info("request 1: Find the model number, speed and hard drive capacity for all the PCs with prices below $500."
         + " Result set: model, speed, hd.");
     Table request1Table = DataBaseUtil.getRequest(DataManager.getRequestData("request1"));
     Assert.assertEquals(request1Table, JsonUtil.getTableFromFile("request1ExpectedTable"),
-        "actual and expected table  is not equals!");
+        "tables is not equals!");
     MyLogger.info("request 1 Table:\n" + request1Table.getTable());
 
     MyLogger.info("request 2:  Find the makers of PCs with a processor speed of 450 MHz or more. Result set: maker.");
     Table request2Table = DataBaseUtil.getRequest(DataManager.getRequestData("request2"));
     Assert.assertEquals(request2Table, JsonUtil.getTableFromFile("request2ExpectedTable"),
-        "actual and expected table  is not equals!");
+        "tables is not equals!");
     MyLogger.info("request 2 Table:\n" + request2Table.getTable());
 
+    MyLogger.info("request 3:  Get the laptop models that have a speed smaller than the speed of any PC.\n"
+        + "Result set: type, model, speed.");
+    Table request3Table = DataBaseUtil.getRequest(DataManager.getRequestData("request3"));
+    Assert.assertEquals(request3Table, JsonUtil.getTableFromFile("request3ExpectedTable"),
+        "tables is not equals!");
+    MyLogger.info("request 3 Table:\n" + request3Table.getTable());
+    MyLogger.info("DbTest finish");
   }
 }
